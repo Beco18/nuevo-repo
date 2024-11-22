d69d2c2b441a5031e9f00ee92c47524c87fd142a
Beco18 <jamesspady18@gmail.com>
2024-11-22 13:23:35 -0300

    m2

--- front/scripts/index.js
+++ front/scripts/index.js
@@ -1,1 +1,34 @@
 console.log(tempData);
+
+const createMoviesCart = ({
+  title,
+  year,
+  director,
+  duration,
+  genre,
+  rate,
+  poster,
+}) => {
+  const tarjeta = document.createElement("div");
+  tarjeta.innerHTML = `
+        <img src="${poster}" alt="pelicula 1">
+        <h3>${title}</h3>
+        <span>${director}</span>
+        <p>${year}</p>
+        <p>${duration}</p>
+        <p>${genre}</p>
+        <p>${rate}</p>`
+
+  return tarjeta;
+};
+
+const renrizarCart = () => {
+  const containair = document.getElementById("containair-tarjetas");
+  if (!containair) {
+    return;
+  }
+  tempData.forEach((movies) => {
+    return containair.appendChild(createMoviesCart(movies));
+  });
+};
+renrizarCart();
